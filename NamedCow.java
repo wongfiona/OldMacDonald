0255d25d716e30984a956212ed397f77440669bf
unknown <fiwong2@697-334-Thorin.sf.k12.internal>
2015-10-29 09:50:42 -0700

    farm

--- NamedCow.java
+++ NamedCow.java
@@ -1,4 +1,18 @@
-class NamedCow
+class NamedCow extends Cow
 {
+		private String myName;    
 
+    public NamedCow(String type, String name, String sound)     
+    {   
+        myType = type;         
+        mySound = sound;     
+        myName = name;      
+    }     
+
+    public NamedCow()     
+    {          
+        myName = "Elsie";          
+    }      
+
+    public String getName(){return myName;} 
 }
--- OldMacDonald.pde
+++ OldMacDonald.pde
@@ -1,6 +1,7 @@
 
-void setup()
+public void setup()
 {
-	//your variable declarations here
+	Farm d = new Farm();
+    d.animalSounds();
 }
 
